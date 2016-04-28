04bf0b05182d31a2cefab310c709a6990352fa4e
Alvin Thai <athai005@ucr.edu>
2016-04-27 19:40:38 -0700

    wait syscall hotfix, beginning waitpid

--- proc.c
+++ proc.c
@@ -241,7 +241,7 @@ wait(int *status)
         p->parent = 0;
         p->name[0] = 0;
         p->killed = 0;
-//	if (status) *status = p->exitstat;
+	if (status > 0) *status = p->exitstat;
         release(&ptable.lock);
         return pid;
       }
@@ -258,7 +258,50 @@ wait(int *status)
     sleep(proc, &ptable.lock);  //DOC: wait-sleep
   }
 }
+/*
+int
+waitpid (int pid, int *status, int options)
+{
+  struct proc *p;
+  int havekids, zpid;
 
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for zombie children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != proc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        zpid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->state = UNUSED;
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+//	if (status) *status = p->exitstat;
+        release(&ptable.lock);
+        return zpid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || proc->killed){
+      *status = -1;
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(proc, &ptable.lock);  //DOC: wait-sleep
+  }
+}
+*/
 //PAGEBREAK: 42
 // Per-CPU process scheduler.
 // Each CPU calls scheduler() after setting itself up.
